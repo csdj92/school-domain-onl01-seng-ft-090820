34d636e0b762c2135ff0eb9d1e2979fef9b106eb
CJ Davis <csdj92@gmail.com>
2020-09-14 16:22:03 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -15,4 +15,12 @@ def initialize(name)
     def grade
       roster[grade_level]
     end
+    
+    def sort 
+      sort={}
+      roster.each do |grade_level, student|
+        sort[grade_level] = student.sort
+      end
+      sort
+    end
 end
